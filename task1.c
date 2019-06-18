9d76497d5147ea6d2b2a0eeaa7203c745cadd4f2
JYOTI3118 <44294378+JYOTI3118@users.noreply.github.com>
2019-06-18 22:49:11 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char name[100];
+int age;
+int no;
+printf("enter your name, age, phone number");
+scanf("%s%d%d" ,&name , &age , &no);
+printf("%s%d%d", name , age , no);
+
+	getch();
+}
+
